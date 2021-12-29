f5d16163a3245fdcc7990f4b9442613054e2a05d
Julia Ziębińska <julkazbs2000@gmail.com>
2021-12-29 17:48:59 +0100

    changes

--- apps/admin/.eslintrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('@przewodnik/config/eslint-preset')
--- apps/admin/next.config.js
+++
@@ -1,6 +0,0 @@
-const withTM = require('next-transpile-modules')(['@przewodnik/ui'])
-
-module.exports = withTM({
-  reactStrictMode: true,
-  images: { domains: ['images.unsplash.com'] },
-})

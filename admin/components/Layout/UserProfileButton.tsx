31d4ed3ff4257dc657d819c9e2e863ffeb9e38cd
Oxygen <66377130+karolstawowski@users.noreply.github.com>
2022-01-12 11:20:59 +0100

    feat: add internationalization (#7)

--- admin/components/Layout/UserProfileButton.tsx
+++ admin/components/Layout/UserProfileButton.tsx
@@ -1,25 +1,30 @@
 import Image from 'next/image'
+import useTranslation from 'next-translate/useTranslation'
 
-export const UserProfileButton = () => (
-  <div className="flex-shrink-0 flex bg-gray-700 p-4">
-    <a href="#" className="flex-shrink-0 w-full group block">
-      <div className="flex items-center">
-        <div>
-          <Image
-            className="inline-block h-9 w-9 rounded-full"
-            width={48}
-            height={48}
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
+export const UserProfileButton = () => {
+  const { t } = useTranslation()
+
+  return (
+    <div className="flex-shrink-0 flex bg-gray-700 p-4">
+      <a href="#" className="flex-shrink-0 w-full group block">
+        <div className="flex items-center">
+          <div>
+            <Image
+              className="inline-block h-9 w-9 rounded-full"
+              width={48}
+              height={48}
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-base font-medium text-white">Jan Nowak</p>
+            <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">
+              {t('global:userProfile')}
+            </p>
+          </div>
         </div>
-        <div className="ml-3">
-          <p className="text-base font-medium text-white">Jan Nowak</p>
-          <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">
-            Profil użytkownika
-          </p>
-        </div>
-      </div>
-    </a>
-  </div>
-)
+      </a>
+    </div>
+  )
+}
--- admin/pages/guide.tsx
+++ admin/pages/guide.tsx
@@ -1,19 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Layout from '../components/Layout/Layout'
+import useTranslation from 'next-translate/useTranslation'
 
 const Home: NextPage = () => {
+  const { t } = useTranslation()
+
   return (
     <>
       <Head>
-        <title>Przewodnik | Przewodnik</title>
+        <title>
+          {t('guide:guideTitle')} | {t('global:webpageTitle')}
+        </title>
         <meta
           name="description"
           content="Aplikacja do zarządzania przewodnikiem"
         />
       </Head>
 
-      <Layout title="Przewodnik">
+      <Layout title={t('guide:guideTitle')}>
         <div>Generowanie i personalizacja przewodnika</div>
       </Layout>
     </>
